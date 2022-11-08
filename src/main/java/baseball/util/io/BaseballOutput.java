d727cbe78d19b1ae99cebabfa876739861679e9f
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-08 16:28:28 +0900

    feat(util/io/BaseballOutput.java): 숫자 야구 게임 시작, 결과, 결과 성공을 출력
    숫자 야구 게임에 필요한 출력을 하는 역할
    - 숫자 야구 게임 시작(맨 처음 초기화)
    - 숫자 야구 게임 도중 현재 결과 출력
    - 결과가 성공했으면 출력
    
    [skip ci]

---
+++ src/main/java/baseball/util/io/BaseballOutput.java
@@ -0,0 +1,18 @@
+package baseball.util.io;
+
+import baseball.domain.Baseball;
+
+public class BaseballOutput {
+
+    public static void printInit() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printResult(Baseball baseball) {
+        System.out.println(baseball.toString());
+    }
+
+    public static void printSuccess() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+}
