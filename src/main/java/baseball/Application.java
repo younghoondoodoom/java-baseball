636814e21a27e1b11ab98557998a28dbcd4022f9
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-08 16:58:59 +0900

    feat(Application.java): 숫자 야구 게임 시작 포인트 생성
    숫자 야규 게임 시작 포인트
    - BaseballConfig를 통해 게임 시작 포인트 생성
    
    [skip ci]

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.config.BaseballConfig;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballConfig.config().play();
     }
 }
