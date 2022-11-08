524bf5d15da63ba33269f17b9d2f8c2b8315b505
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-08 16:11:03 +0900

    feat(service/BaseballServiceImpl.java): judge 메소드 구현
    입력 값 리스트 두개의 크기가 다르면 IllegalArgumentException을 던짐
    볼, 스트라이크 개수 판단
    
    [skip ci]

---
+++ src/main/java/baseball/service/BaseballServiceImpl.java
@@ -0,0 +1,38 @@
+package baseball.service;
+
+import baseball.domain.Baseball;
+import java.util.List;
+
+public class BaseballServiceImpl implements BaseballService {
+
+    @Override
+    public Baseball judge(final List<Integer> numbers, final List<Integer> goal) {
+        if (numbers.size() != goal.size()) {
+            throw new IllegalArgumentException();
+        }
+        int strikeCount = countStrike(numbers, goal);
+        int ballCount = countBall(numbers, goal, strikeCount);
+        return new Baseball(strikeCount, ballCount);
+    }
+
+    private int countStrike(final List<Integer> numbers, final List<Integer> goal) {
+        int count = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i).equals(goal.get(i))) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private int countBall(final List<Integer> numbers, final List<Integer> goal,
+        final int strikeCount) {
+        int count = 0;
+        for (Integer number : numbers) {
+            if (goal.contains(number)) {
+                count++;
+            }
+        }
+        return count - strikeCount;
+    }
+}
