8e84c0175dcc100f8738ba470d4a4b0059a201f9
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-08 16:23:46 +0900

    feat(validation/BaseballInputValidator.java): 숫자 야구 게임 입력값이 유효한지 검사하는 메소드 구현
    값이 기준에 유효한지 검사
    - 크기가 3인지 검사
    - 숫자 범위가 1~9인지 검사
    - 중복된 숫자가 있는지 검사
    
    [skip ci]

---
+++ src/main/java/baseball/validation/BaseballInputValidator.java
@@ -0,0 +1,51 @@
+package baseball.validation;
+
+import java.util.List;
+
+public class BaseballInputValidator implements Validator<List<Integer>> {
+
+    private static final int SIZE = 3;
+
+    @Override
+    public boolean validate(List<Integer> target) {
+        return validateSize(target) && validateRange(target, 1, 9) && validateDup(target);
+    }
+
+    private boolean validateSize(final List<Integer> target) {
+        return target.size() == SIZE;
+    }
+
+    private boolean validateRange(final List<Integer> target,
+        final int startInclusive,
+        final int endInclusive) {
+        for (Integer num : target) {
+            if (num < startInclusive || num > endInclusive) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean validateDup(final List<Integer> target) {
+        for (int i = 0; i < target.size(); i++) {
+            int cur = target.get(i);
+            if (isDup(target, cur, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isDup(final List<Integer> target, final int cur,
+        final int curIdx) {
+        for (int i = 0; i < target.size(); i++) {
+            if (curIdx == i) {
+                continue;
+            }
+            if (target.get(i).equals(cur)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
