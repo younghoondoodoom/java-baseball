a05fde7123d93b131d626659b9109f18a59a39d8
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-08 15:35:10 +0900

    feat(domain/Baseball.java): 숫자 야구 게임 도메인 모델 생성
    구성 필드
    - 볼 개수
    - 스트라이크 개수
    구성 메소드
    - getter
    - allargsconstructor
    - 출력 형식을
    맞추기 위한 toString
    
    [skip ci]

---
+++ src/main/java/baseball/domain/Baseball.java
@@ -0,0 +1,40 @@
+package baseball.domain;
+
+public class Baseball {
+
+    private final int strike;
+    private final int ball;
+
+    public Baseball(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    @Override
+    public String toString() {
+        if (strike == 0 && ball == 0) {
+            return "낫싱";
+        } else if (strike == 0) {
+            return toUnitExpression(ball, "볼");
+        } else if (ball == 0) {
+            return toUnitExpression(strike, "스트라이크");
+        } else {
+            return toUnitExpression(ball, "볼")
+                + " "
+                + toUnitExpression(strike, "스트라이크");
+        }
+    }
+
+    private String toUnitExpression(int number, String unit) {
+        return number + unit;
+    }
+
+}
